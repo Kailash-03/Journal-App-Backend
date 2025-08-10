701b2562a5cfd760c6af659cff75cef883096f76
Kailash  Chandra  Sharma <kailash.sharma@infoedge.com>
2025-08-10 12:25:45 +0530

    port change

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ import {app} from './app.js';
     connectDB();
 
 
-    app.listen(3000, () => { 
+    app.listen(process.env.port, () => { 
     console.log("Server is running on port 3000");
     });
